4eb9375da25e5d1d2c607118f05895e2190acf77
taejin-seong <sungtj0130@naver.com>
2023-05-01 21:27:40 +0900

    STM32C0316-DK #9

---
+++ STM32C0/stm32c0_pwm/App/include/def.h
@@ -0,0 +1,31 @@
+/*
+ * def.h
+ *
+ *  Created on: Apr 15, 2023
+ *      Author: xowls
+ */
+
+#ifndef DEF_H_
+#define DEF_H_
+
+
+#include <stdbool.h>
+#include <stdint.h>
+
+#include "main.h"
+
+
+#define _DEF_CH1         0
+#define _DEF_CH2         1
+#define _DEF_CH3         2
+#define _DEF_CH4         3
+
+
+
+
+
+void delay(uint32_t time_ms);
+uint32_t millis(void);
+
+
+#endif /* DEF_H_ */
